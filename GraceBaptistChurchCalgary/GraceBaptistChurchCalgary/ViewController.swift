8f2cce36c69efe55466fdec4525985e7a0da86d8
dineshabe <dineshabe@gmail.com>
2016-01-13 22:39:14 -0700

    initial commit

---
+++ GraceBaptistChurchCalgary/GraceBaptistChurchCalgary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraceBaptistChurchCalgary
+//
+//  Created by Dinesh Thangasamy on 2016-01-13.
+//  Copyright © 2016 GBCCalgary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
